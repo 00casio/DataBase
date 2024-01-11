23a20184d9774a5a7ecbc37f1e39c7157ac7ee72
jadalaou <hamza.jad-al-aoun@eurecom.fr>
2024-01-11 18:27:43 +0100

    delete ready and pass tests

--- Lab1-starter-code/src/java/simpledb/Delete.java
+++ Lab1-starter-code/src/java/simpledb/Delete.java
@@ -9,7 +9,11 @@ import java.io.IOException;
 public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
-
+    private TransactionId t;
+    private OpIterator child;
+    private TupleDesc tpd;
+    private boolean flag;
+    
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
@@ -20,24 +24,35 @@ public class Delete extends Operator {
      *            The child operator from which to read tuples for deletion
      */
     public Delete(TransactionId t, OpIterator child) {
-        // some code goes here
+        this.t = t;
+        this.child = child;
+        Type[] typeAr = new Type[1];
+        typeAr[0] = Type.INT_TYPE;
+        String[] fieldAr = new String[1];
+        fieldAr[0] = "countInsertedTuples";
+        this.tpd = new TupleDesc(typeAr, fieldAr);
+        this.flag= false;
+        
+
+    
     }
 
     public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
+        return this.child.getTupleDesc();
     }
 
     public void open() throws DbException, TransactionAbortedException {
-        // some code goes here
+        super.open();
+        this.child.open();
     }
 
     public void close() {
-        // some code goes here
+        super.close();
+        this.child.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
-        // some code goes here
+        this.child.rewind();
     }
 
     /**
@@ -50,19 +65,41 @@ public class Delete extends Operator {
      * @see BufferPool#deleteTuple
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-        // some code goes here
+        if(!this.flag){
+        int deletedCount = 0;
+        if(this.child.hasNext()){
+        
+        while (this.child.hasNext()) {
+        Tuple tuple = this.child.next();
+            try {
+                Database.getBufferPool().deleteTuple(this.t, tuple);
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            deletedCount++;
+        }
+    }
+        this.flag = true;
+        Tuple resultTuple = new Tuple(this.tpd);
+        resultTuple.setField(0, new IntField(deletedCount));
+        return resultTuple;
+    }
+    else 
         return null;
     }
+    
 
     @Override
     public OpIterator[] getChildren() {
-        // some code goes here
-        return null;
+        OpIterator[] returnvalue = new OpIterator[1];
+        returnvalue[0] = this.child;
+        return returnvalue;
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
-        // some code goes here
+        this.child = children[0];
     }
 
 }
